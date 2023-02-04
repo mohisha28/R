c93172b31a97d9653b09bbf5e2259f575f95245b
Mohisha Gunturu <98393079+mohisha28@users.noreply.github.com>
2023-02-04 21:01:39 +0530

    Create lab1.r

---
+++ lab1.r
@@ -0,0 +1,17 @@
+# Enter the data {1, 2, .... ,19,20} in a variable 𝑥.
+x = c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20)
+
+# Find the 3rd element in the data list.
+x[3]
+
+# Find 3rd to 5th element in the data list.
+x[3:5]
+
+# Find 2nd, 5th, 6th, and 12th element in the list.
+x[2]
+x[5]
+x[6]
+x[12]
+
+# Print the data as {20, 19, ..., 2, 1} without again entering the data.
+x[20:1]
